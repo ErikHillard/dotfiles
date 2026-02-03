304238405cff87603fb23bf9b5fd6d752c51345d
Erik Hillard <erikhillard2018@gmail.com>
2026-02-03 06:49:28 -0500

    feat: adding new neovim plugins

---
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,35 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lint = require("lint")
+
+    -- Force shellcheck to use stdin
+    -- This might not be needed in the future, but it is for now sadness
+    lint.linters.shellcheck.stdin = true
+    lint.linters.shellcheck.args = {
+      "--format",
+      "json1",
+      "-", -- This tells shellcheck to read from stdin
+    }
+
+    -- Configure linters by filetype
+    lint.linters_by_ft = {
+      -- Add linters you want to use
+      -- python = { "pylint" },
+      -- javascript = { "eslint_d" },
+      sh = { "shellcheck" },
+      markdown = { "markdownlint-cli2" },
+    }
+
+    -- Auto-lint on save and when entering buffer
+    local lint_augroup = vim.api.nvim_create_augroup("nvim-lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "TextChanged", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        require("lint").try_lint()
+      end,
+    })
+  end,
+}
